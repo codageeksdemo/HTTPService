6a5ac2ffd6ee3761d68a9f0a6a4767f8915618d2
codageeksdemo <support@codageeks.com>
2022-06-02 00:26:42 +0530

    Automation Microservice Code

--- src/main/java/com/miracle/muletransform/microservice/controller/DefaultController.java
+++ src/main/java/com/miracle/muletransform/microservice/controller/DefaultController.java
@@ -1,8 +1,14 @@
 package com.miracle.muletransform.microservice.controller;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.miracle.labs.microservice.transform.web.model.TransformRequest;
 
 @RestController()
 @RequestMapping("${default.controller.path.map}")
@@ -12,5 +18,12 @@ class DefaultController {
   
   String home() {
     return "Hello, Welcome to Mule Transform demo! ";
+  }
+  
+  @PutMapping(path = {"/"}, consumes = {"application/json"})
+  String receive(@RequestBody String request) throws ClassNotFoundException, IOException, InterruptedException {
+    
+    
+    return "success";
   }
 }
